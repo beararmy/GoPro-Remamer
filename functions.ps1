73812b1aebbe0725bf56f42116193025a175546d
Stef Harrington-Palmer <stef@bear.army>
2020-05-13 15:57:41 +0100

    working!

--- functions.ps1
+++ functions.ps1
@@ -1,7 +1,7 @@
-function Find-PossibleSequences {
+function Find-GoProPossibleSequences {
     param (
         [string]$workingFolder = ".",
-        [string]$cameraModel,
+        [string]$cameraModel = "HERO5",
         [string]$sequenceNumber
 
     )
@@ -9,24 +9,25 @@ function Find-PossibleSequences {
         HERO5 { $sequenceNumberRegex = "GOPR*MP4" }
     }
 
-    if ($sequenceNumber) {
-        $list = Get-ChildItem -Path $workingFolder -Filter $sequenceNumberRegex | Where-Object { $_.Name -match "$sequenceNumber" }
-        $nameString = $list.Name.ToString()
-        $sequenceNumber = ($nameString).SubString(4, 4)
-        $fullWorkingPath = $workingFolder + $list.Name
-        [pscustomobject]$output = [PSCustomObject]@{
-            SequenceNumber   = $sequenceNumber
-            SequenceFilename = $list.Name
-            FullPath         = $fullWorkingPath
-            var4 = "test"
+    # if ($sequenceNumber) {
+    #     $list = Get-ChildItem -Path $workingFolder -Filter $sequenceNumberRegex | Where-Object { $_.Name -match "$sequenceNumber" }
+    #     $nameString = $list.Name.ToString()
+    #     $sequenceNumber = ($nameString).SubString(4, 4)
+    #     $fullWorkingPath = $workingFolder + $list.Name
+    #     [pscustomobject]$output = [PSCustomObject]@{
+    #         SequenceNumber   = $sequenceNumber
+    #         SequenceFilename = $list.Name
+    #         FullPath         = $fullWorkingPath
+    #         var4 = "test"
 
-        }
-        return $output
-    } else {
-        Write-Error "Failed to identify any Sequences"
-        return $false
-    }
+    #     }
+    #     return $output
+    # } else {
+    #     Write-Error "Failed to identify any Sequences"
+    #     return $false
+    # }
 
+    # Find possible sequences
     $list = Get-ChildItem -Path $workingFolder -Filter $sequenceNumberRegex 
     [pscustomobject]$output = foreach ($listitem in $list) {
         $nameString = $listitem.Name.ToString()
@@ -37,58 +38,57 @@ function Find-PossibleSequences {
             SequenceNumber   = $sequenceNumber
             SequenceFilename = $listitem.Name
             FullPath         = $fullWorkingPath
-            var4 = "test"
+            #var4 = "test"
 
         }
     }
     return $output
 }
-
-
-function Find-SequenceChildren {
+function Find-GoProSequenceChildren {
     param (
-        [string]$workingFolder = ".",
+        [string]$workingFolder,
         [string]$cameraModel,
-        [string]$sequenceNumber,
-        [string]$sequenceFilename,
-        [bool]$includeParent = $true,
-        [parameter(ValueFromPipeline = $true)]$pipelineInput
-    )
-$pipelineInput
+        [string]$sequenceNumber
+    )   
     switch ( $cameraModel ) {
-        HERO5 { $sequenceString = ("GP*" + $sequenceNumber + ".MP4") }
-    }
-    Write-Verbose "Found $($listitem.Name) as Sequence number $sequenceNumber. File is at $fullWorkingPath"
-    if ($includeParent) {
-        $output1 = Find-PossibleSequences -workingFolder $workingfolder -cameraModel $cameraModel -sequenceNumber $sequenceNumber
-    }
-    $list = Get-ChildItem -Path $workingFolder -Filter $sequenceString
-    [pscustomobject]$output2 = foreach ($listitem in $list) {
-        $fullWorkingPath = $workingFolder + $listitem.Name
-        Write-Verbose "Found $($listitem.Name). File is at $fullWorkingPath"
-        [PSCustomObject]@{
-            SequenceNumber   = $sequenceNumber
-            SequenceFilename = $listitem.Name
-            FullPath         = $fullWorkingPath
-            var4 = "test"
-
+        HERO5 {
+            $sequenceChildrenRegex = ( "GP*" + $sequenceNumber + ".MP4")
+            $sequenceParentalRegex = "GOPR*MP4" 
         }
     }
-
-$object3 = "boobs"
-[pscustomobject]$return = $object1 + $object2
-    #return $output1, $output2, $object3
-    return $return
+    $filelist = Get-ChildItem -Path $workingFolder -Filter $sequenceParentalRegex | Where-Object { $_.Name -match "$sequenceNumber" }
+    $nameString = $filelist.Name.ToString()
+    $fullWorkingPath = $workingFolder + $filelist.Name
+    Write-Host "Found $nameString. File is at $fullWorkingPath"
+    [int]$childCounter = 1
+    $parentfile = [pscustomobject]@{
+        FileNumber       = $childCounter
+        SequenceNumber   = $sequenceNumber
+        SequenceFilename = $nameString
+        FullPath         = $fullWorkingPath
+    }
+    $childCounter++
+    $filelist = Get-ChildItem -Path $workingFolder -Filter $sequenceChildrenRegex
+    $childrenfiles = foreach ( $file in $filelist ) {
+        $fullWorkingPath = $workingFolder + $file.Name
+        Write-Verbose "Found $($file.Name). File is at $fullWorkingPath"
+        [PSCustomObject]@{
+            FileNumber       = $childCounter
+            SequenceNumber   = $sequenceNumber
+            SequenceFilename = $file.Name
+            FullPath         = $fullWorkingPath
+        }
+        $childCounter++
+    }
+    $parentfile
+    $childrenfiles
 }
 
 
+[string]$workingFolder = "C:\Users\msn\Desktop\testfolder\"
+[string]$cameraModel = "HERO5"
+[string]$sequenceNumber = "0548"
 
-$workingFolder = "D:\03. Go Pro\2020-04-05 - Cycle - to Rackspace and back\"
-$cameraModel = "HERO5"
+Find-GoProSequenceChildren -workingFolder $workingFolder -cameraModel $cameraModel -sequenceNumber 0548
 
-# Find-PossibleSequences -workingFolder $workingfolder -cameraModel $cameraModel
-# Find-PossibleSequences -workingFolder $workingfolder -cameraModel $cameraModel -sequenceNumber 0533
-
-$files = ( Find-SequenceChildren -workingFolder $workingfolder -cameraModel $cameraModel -sequenceNumber 0532 )
-
-Write-Verbose "I found $($files.count) files to process"
+Find-GoProPossibleSequences -workingFolder $workingfolder -cameraModel $cameraModel
